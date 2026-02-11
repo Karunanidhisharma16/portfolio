b351b9acf04d1c1f5c852f92f87724574d8bd25e
Karunanidhi Sharma <Karunanidhisharma16@users.noreply.github.com>
2026-02-12 01:03:45 +0530

    Update project data with new titles, descriptions, and links for SignSureWeb3 and ChatConnect

--- src/data/projectdata.js
+++ src/data/projectdata.js
@@ -3,31 +3,36 @@
 const projectsData = [
   {
     id: 1,
-    title: "Project 1",
-    description: "soon to be publish...",
-    tech: ["React", "Node.js", "MongoDB", "Express"],
+    title: "SignSureWeb3",
+    description: "Transaction Safety wallet",
+    tech: ["React", "Ethers.js", "Tailwind CSS", "MetaMask", "Etherscan API"],
+//     tech: {
+//   frontend: ["React", "Tailwind CSS", "Vite"],
+//   blockchain: ["Ethers.js", "MetaMask", "Sepolia"],
+//   apis: ["Etherscan API", "Alchemy"]
+// }
     image: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?w=800&q=80",
-    liveLink: "https://example.com",
+    liveLink: "signsureweb3.logicryx.in",
     githubLink: "https://github.com/Karunanidhisharma16"
   },
   {
     id: 1,
-    title: "Project 2",
-    description: "soon to be publish...",
-    tech: ["React", "Node.js", "MongoDB", "Express"],
+    title: "ChatConnect",
+    description: " real-time messaging platform connecting users instantly",
+    tech: ["React", "Node.js", "MongoDB", "Express","Socket.io"],
     image: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=800&q=80",
     liveLink: "https://example.com",
     githubLink: "https://github.com/Karunanidhisharma16"
   },
-  {
-    id: 3,
-    title: "Project 3",
-    description: "soon to be publish...",
-    tech: ["React", "Node.js", "MongoDB", "Express"],
-    image: "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?w=800&q=80",
-    liveLink: "https://example.com",
-    githubLink: "https://github.com/Karunanidhiharma16"
-  },
+  // {
+  //   id: 3,
+  //   title: "Project 3",
+  //   description: "soon to be publish...",
+  //   tech: ["React", "Node.js", "MongoDB", "Express"],
+  //   image: "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?w=800&q=80",
+  //   liveLink: "https://example.com",
+  //   githubLink: "https://github.com/Karunanidhiharma16"
+  // },
 
 
 
