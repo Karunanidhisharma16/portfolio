ac138f99b0051841d6e89059aa4a137da659288b
Karunanidhi Sharma <Karunanidhisharma16@users.noreply.github.com>
2026-02-13 23:30:08 +0530

    Refactor code structure for improved readability and maintainability

--- src/data/projectdata.js
+++ src/data/projectdata.js
@@ -11,7 +11,7 @@ const projectsData = [
 //   blockchain: ["Ethers.js", "MetaMask", "Sepolia"],
 //   apis: ["Etherscan API", "Alchemy"]
 // }
-    image: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?w=800&q=80",
+    image: "/signsure.png",
     liveLink: "signsureweb3.logicryx.in",
     githubLink: "https://github.com/Karunanidhisharma16"
   },
